1946d5f7a8313fb935b02aeffaf7fa027cc93c37
Yoad Abels <yoad.abels@gmail.com>
2022-03-11 10:49:34 +0200

    fixed some bugs

--- db_server.py
+++ db_server.py
@@ -62,6 +62,6 @@ if __name__ == '__main__':
     firebase_admin.initialize_app(cred)
     db = firestore.client()
     if not (try_open_db(db)): quit()
-    db_server('172.23.160.1', PORT, db).start()
+    db_server('172.22.208.1', PORT, db).start()
 
 # 200#{cameraId}#{time1}#{time2}
